02f4f0ac0f31c48d8e6b26f9608fd97495e2a0f1
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2022-04-21 16:00:16 +0100

    added

---
+++ contract/index.js
@@ -0,0 +1,3 @@
+export { default as ELECTION_ABI } from './SimpleElection.json';
+export const ELECTION_ADDRESS = '0xD537571BAfaf8c02fE8310Db64d3cF390904c860';
+ 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import { ToastContainer } from 'react-toastify';
+// import 'styles/globals.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div>
+    <Component {...pageProps} />
+  <ToastContainer
+  position='top-right'
+  autoClose={5000}
+  hideProgressBar={false}
+  newestOnTop={false}
+  closeOnClick
+  rtl={false}
+  pauseOnFocusLoss
+  draggable
+  pauseOnHover
+  theme='light'
+/>
+  </div>
 }
 
 export default MyApp
