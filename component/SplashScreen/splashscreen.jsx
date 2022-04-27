81b115ad6f913f9cc095fe03c0b356c52c5ca8cd
James Harrison <somtochukwujames2000@gmail.com>
2022-04-27 01:34:01 +0100

    refactor: changed folder structure and added splashscreen

---
+++ component/SplashScreen/splashscreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Image from 'next/image';
+import styles from "./splashscreen.module.css";
+
+export default function Splashscreen ()
+{
+    return (
+        <div className={ styles.splashscreen }>
+            <h1>ZE</h1>
+        </div>
+    )
+}
